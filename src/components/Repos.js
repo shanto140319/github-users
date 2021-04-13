a5841e2279e1a407ad1d95e9b745cc47ac3e6b2a
shanto140319 <shanto140319@gmail.com>
2021-04-13 19:31:00 +0600

    first commit

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,80 @@
 import React from 'react';
 import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import { useGlobalContext } from '../context/context';
+import { Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useGlobalContext();
+
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
+    }
+    return total;
+  }, {});
+
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+
+  //stars and forks
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, forks, name } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+  console.log(stars);
+  console.log(forks);
+
+  const chartData = [
+    {
+      label: 'Html',
+      value: '290',
+    },
+    {
+      label: 'Css',
+      value: '260',
+    },
+    {
+      label: 'JavaScript',
+      value: '180',
+    },
+  ];
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Pie3D data={mostUsed} />
+        <Column3D data={chartData} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={chartData} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+import React from 'react'
+import { Info, Repos, User, Search, Navbar } from '../components'
+import loadingImage from '../images/preloader.gif'
+import { GithubContext, useGlobalContext } from '../context/context'
 const Dashboard = () => {
+  const data = useGlobalContext()
+  console.log(data)
   return (
     <main>
-      <h2>Dashboard Page</h2>
+      {/* <Navbar />
+      <Search /> */}
+      <Info />
+      <User />
+      <Repos />
     </main>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
